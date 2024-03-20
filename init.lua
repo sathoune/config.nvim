aaf9d5d9fef7ccf0a22e5e6c369d283f478d0f6a
sathoune <michal5bargiel@gmail.com>
2024-03-20 22:37:31 +0100

    ➕ Add nvim-nio and upgrade packages.

--- init.lua
+++ init.lua
@@ -222,7 +222,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'nvim-neotest/nvim-nio',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
