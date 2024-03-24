e7c88e43a5a9fa2837cdedcd4fc290d2278e0b86
sathoune <michal5bargiel@gmail.com>
2024-03-24 17:27:52 +0100

    💄 Set priority for theme loading.

--- init.lua
+++ init.lua
@@ -638,7 +638,9 @@ require('lazy').setup({
   {
     -- Colorscheme
     'rose-pine/neovim',
-    name = 'rose-pine-theme',
+    name = 'rose-pine',
+    lazy = false,
+    priority = 1000,
     config = function()
       vim.cmd.colorscheme 'rose-pine-main'
       vim.cmd.hi 'Comment gui=none'
