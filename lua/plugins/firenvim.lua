a10b8d5293680aa3dae0f725a3fb008a297d2865
sathoune <michal5bargiel@gmail.com>
2024-03-28 00:23:55 +0100

    ✨ Increase minimal number of lines for `firenvim` instances.

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -16,8 +16,19 @@ local Module = {
 
   config_overrides = function()
     if vim.g.started_by_firenvim then
-      vim.opt.number = false
-      vim.opt.relativenumber = false
+      vim.api.nvim_create_autocmd({ 'UIEnter' }, {
+        callback = function()
+          vim.opt.laststatus = 0
+          vim.opt.relativenumber = false
+
+          local i_prefer_more_lines_than = 5
+          local desired_lines_number = 6
+          local current_lines = vim.opt.lines['_value']
+          if i_prefer_more_lines_than >= current_lines then
+            vim.opt.lines = desired_lines_number
+          end
+        end,
+      })
     end
   end,
 
