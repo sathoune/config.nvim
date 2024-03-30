60a7d26491ce23af7934a371c36b876343a7aca7
sathoune <michal5bargiel@gmail.com>
2024-03-30 23:13:32 +0100

    🚚 Move the rest of the plugins.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,14 @@
 local Module = {
+  require('plugins.misc').lazy_config,
   require('plugins.colorscheme').lazy_config,
   require('plugins.firenvim').lazy_config,
   require('plugins.dap.main').lazy_config,
   require('plugins.telescope').lazy_config,
   require('plugins.lsp').lazy_config,
   require('plugins.completion').lazy_config,
+  require('plugins.conform').lazy_config,
+  require('plugins.mini').lazy_config,
+  require('plugins.treesitter').lazy_config,
 }
 
 return Module
