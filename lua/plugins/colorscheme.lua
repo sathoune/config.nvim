f365e2a9ee752999edd00e86c995cb1d5040d7ed
sathoune <michal5bargiel@gmail.com>
2024-03-31 13:12:22 +0200

    💄 Set background to inherit from terminal.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,12 @@ local Module = {
   lazy = false,
   priority = 1000,
   config = function()
+    require('rose-pine').setup {
+      styles = {
+        transparency = true,
+      },
+    }
+
     vim.cmd.colorscheme 'rose-pine-main'
     vim.cmd.hi 'Comment gui=none'
   end,
