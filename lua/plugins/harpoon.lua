fde4820f7b8e0ab446a0cd4d695d976d1b54b6fc
sathoune <michal5bargiel@gmail.com>
2024-04-06 14:06:16 +0200

    🔥 Drop telescope integration.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 local Module = {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local harpoon = require 'harpoon'
         harpoon:setup()
@@ -32,30 +32,6 @@ local Module = {
         end)
         vim.keymap.set('n', '<C-S-N>', function()
             harpoon:list():next()
-        end)
-
-        -- Configure Telescope UI
-        local telescope_conf = require('telescope.config').values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
-
-            require('telescope.pickers')
-                .new({}, {
-                    prompt_title = 'Harpoon',
-                    finder = require('telescope.finders').new_table {
-                        results = file_paths,
-                    },
-                    previewers = telescope_conf.file_previewer {},
-                    sorter = telescope_conf.generic_sorter {},
-                })
-                :find()
-        end
-
-        vim.keymap.set('n', '<C-e>', function()
-            toggle_telescope(harpoon:list())
         end)
     end,
 }
