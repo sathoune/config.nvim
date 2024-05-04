c09b305141a8d4cff4d7bd196ac285e77f2a19bb
sathoune <michal5bargiel@gmail.com>
2024-05-04 23:33:29 +0200

    ♻️ Don't close dap-ui on script finishing the run.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -35,21 +35,28 @@ local Module = {
             ensure_installed = { 'delve' },
         }
 
+        local dap = require 'dap'
+        local dapui = require 'dapui'
         vim.keymap.set(
             'n',
             '<leader>db',
             '<cmd> DapToggleBreakpoint <CR>',
             { desc = '[D]ap Toggle [B]reakpoint' }
         )
+        vim.keymap.set('n', '<leader>dn', '<cmd> DapContinue <CR>', { desc = '[D]ap Co[N]tinue' })
+        vim.keymap.set(
+            'n',
+            '<leader>dr',
+            '<cmd> DapRestartFrame <CR>',
+            { desc = '[D]ap [R]estart' }
+        )
+        vim.keymap.set('n', '<leader>dt', dapui.toggle, { desc = 'Toggle DAP' })
 
-        local dap = require 'dap'
-        local dapui = require 'dapui'
         dapui.setup()
 
         dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-        dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
+        vim.keymap.set('n', '<leader>dq', dapui.close, { desc = '[D]ap [Q]uit' })
         require('dap-go').setup()
         require('dap-python').setup()
     end,
