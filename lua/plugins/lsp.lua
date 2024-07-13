8a04638db63ba957c737b800b2ab4419fed70221
sathoune <michal5bargiel@gmail.com>
2024-07-13 22:28:04 +0200

    ➖ Drop `pyright`.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -159,7 +159,6 @@ local Module = {
         servers = {
             -- clangd = {},
             gopls = {},
-            pyright = {},
             -- rust_analyzer = {},
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
             --
