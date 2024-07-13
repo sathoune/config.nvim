c243501f688d0ba7c93daee18addaff0d16d782e
sathoune <michal5bargiel@gmail.com>
2024-07-13 22:28:21 +0200

    ➕ Add `basedpyright`.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -159,6 +159,7 @@ local Module = {
         servers = {
             -- clangd = {},
             gopls = {},
+            basedpyright = {},
             -- rust_analyzer = {},
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
             --
