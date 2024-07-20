1e4536812eff8baf93d1efb5c8a0e5b4179bc143
sathoune <michal5bargiel@gmail.com>
2024-07-20 19:32:15 +0200

    🔧 Remap musical keys.

--- lua/plugins/apple-music.lua
+++ lua/plugins/apple-music.lua
@@ -4,39 +4,14 @@ local Module = {
     config = true,
     keys = function()
         local plugin = require 'apple-music'
-        local my_keys = {
-            {
-                '<leader>amp',
-                plugin.toggle_play,
-                desc = 'Toggle [P]layback',
-            },
-            {
-                '<leader>ams',
-                plugin.toggle_shuffle,
-                desc = 'Toggle [S]huffle',
-            },
-            {
-                '<leader>fp',
-                plugin.select_playlist_telescope,
-                desc = '[F]ind [P]laylists',
-            },
-            {
-                '<leader>fa',
-                plugin.select_album_telescope,
-                desc = '[F]ind [A]lbum',
-            },
-            {
-                '<leader>fs',
-                plugin.select_track_telescope,
-                desc = '[F]ind [S]ong',
-            },
-            {
-                '<leader>amx',
-                plugin.cleanup_all,
-                desc = 'Cleanup Temp Playlists',
-            },
+        return {
+            { '<leader>mp', plugin.toggle_play, desc = 'Toggle [P]layback' },
+            { '<leader>ms', plugin.toggle_shuffle, desc = 'Toggle [S]huffle' },
+            { '<leader>mp', plugin.select_playlist_telescope, desc = '[F]ind [P]laylists' },
+            { '<leader>ma', plugin.select_album_telescope, desc = '[F]ind [A]lbum' },
+            { '<leader>ms', plugin.select_track_telescope, desc = '[F]ind [S]ong' },
+            { '<leader>mx', plugin.cleanup_all, desc = 'Cleanup Temp Playlists' },
         }
-        return my_keys
     end,
 }
 
