3730f8300c6e9d96bfcd8bfe4b7e425bdbfde56b
sathoune <michal5bargiel@gmail.com>
2024-07-28 23:46:28 +0200

    🚨 Treat `vim` as global.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -176,18 +176,10 @@ local Module = {
                 -- capabilities = {},
                 settings = {
                     Lua = {
-                        runtime = { version = 'LuaJIT' },
-                        workspace = {
-                            checkThirdParty = false,
-                            -- Tells lua_ls where to find all the Lua files that you have loaded
-                            -- for your neovim configuration.
-                            library = {
-                                '${3rd}/luv/library',
-                                unpack(vim.api.nvim_get_runtime_file('', true)),
-                            },
-                            -- If lua_ls is really slow on your computer, you can try this instead:
-                            -- library = { vim.env.VIMRUNTIME },
+                        diagnostics = {
+                            globals = { 'vim' },
                         },
+                        runtime = { version = 'LuaJIT' },
                         completion = {
                             callSnippet = 'Replace',
                         },
