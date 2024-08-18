59945c513f090fbe811338dbd60314eb798100fd
sathoune <michal5bargiel@gmail.com>
2024-08-18 18:49:38 +0200

    🐛 Fix `rainbow` config not loading at all.

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,11 +1,11 @@
 local Module = {
     'HiPhish/rainbow-delimiters.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    setup = function()
+    config = function()
         -- This module contains a number of default definitions
         local rainbow_delimiters = require 'rainbow-delimiters'
 
-        return {
+        require('rainbow-delimiters.setup').setup {
             strategy = {
                 [''] = rainbow_delimiters.strategy['global'],
                 vim = rainbow_delimiters.strategy['local'],
