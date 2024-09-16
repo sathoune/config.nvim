0fce01626dc9a0027a84fe49473edbfee96823bb
sathoune <michal5bargiel@gmail.com>
2024-09-16 22:49:33 +0200

    🐛 Update `typescript` language server name.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -187,9 +187,7 @@ local Module = {
             --
             -- Some languages (like typescript) have entire language plugins that can be useful:
             --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`tsserver`) will work just fine
-            tsserver = {},
+            ts_ls = {},
             -- dprint = {},
             --
 
