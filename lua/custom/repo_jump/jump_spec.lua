4a05742794dfec9e788aad929cc053bea2529b2b
sathoune <michal5bargiel@gmail.com>
2025-07-12 23:30:53 +0200

    Expect handling line ranges.

--- lua/custom/repo_jump/jump_spec.lua
+++ lua/custom/repo_jump/jump_spec.lua
@@ -10,43 +10,47 @@ local function explode_link(url)
         repo = repo,
         branch = branch,
         file = path,
-        line = tonumber(line),
+        line_start = tonumber(line),
+        line_end = tonumber(line),
     }
 end
 
 describe('Exploding links', function()
     local example_links = {
-        {
+        base_case = {
             url = 'https://github.com/python-poetry/poetry/blob/main/src/poetry/factory.py#L122',
             expected_target = {
                 repo = 'poetry',
                 branch = 'main',
                 file = 'src/poetry/factory.py',
-                line = 122,
+                line_start = 122,
+                line_end = 122,
             },
-            {
-
-                url = 'https://github.com/neovim/neovim/blob/master/src/nvim/buffer.c#L129',
-                expected_target = {
-                    repo = 'neovim',
-                    branch = 'master',
-                    file = 'src/nvim/buffer.c',
-                    line = 129,
-                },
+        },
+        base_case_2 = {
+            url = 'https://github.com/neovim/neovim/blob/master/src/nvim/buffer.c#L129',
+            expected_target = {
+                repo = 'neovim',
+                branch = 'master',
+                file = 'src/nvim/buffer.c',
+                line_start = 129,
+                line_end = 129,
             },
-            {
-                url = 'https://github.com/charmbracelet/bubbletea/blob/main/tea.go#L45',
-                expected_target = {
-                    repo = 'bubbletea',
-                    branch = 'main',
-                    file = 'tea.go',
-                    line = 45,
-                },
+        },
+        multiline = {
+            url = 'https://github.com/charmbracelet/bubbletea/blob/main/tea.go#L45-L57',
+            expected_target = {
+                repo = 'bubbletea',
+                branch = 'main',
+                file = 'tea.go',
+                line_start = 45,
+                line_end = 57,
             },
         },
     }
+
     it('Extracts path elements', function()
-        for _, case in ipairs(example_links) do
+        for _, case in pairs(example_links) do
             local results = explode_link(case.url)
             assert.same(case.expected_target, results)
         end
