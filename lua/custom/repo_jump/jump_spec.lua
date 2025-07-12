47a60888717b5436b51f5bcdb61315e4ad364c27
sathoune <michal5bargiel@gmail.com>
2025-07-12 23:14:29 +0200

    Add two more test cases.

--- lua/custom/repo_jump/jump_spec.lua
+++ lua/custom/repo_jump/jump_spec.lua
@@ -15,17 +15,40 @@ local function explode_link(url)
 end
 
 describe('Exploding links', function()
-    it('Extracts path elements', function()
-        local example_url =
-            'https://github.com/neovim/neovim/blob/master/src/nvim/buffer.c#L129'
-        local expected_target = {
-            repo = 'neovim',
-            branch = 'master',
-            file = 'src/nvim/buffer.c',
-            line = 129,
-        }
+    local example_links = {
+        {
+            url = 'https://github.com/python-poetry/poetry/blob/main/src/poetry/factory.py#L122',
+            expected_target = {
+                repo = 'poetry',
+                branch = 'main',
+                file = 'src/poetry/factory.py',
+                line = 122,
+            },
+            {
 
-        local results = explode_link(example_url)
-        assert.same(expected_target, results)
+                url = 'https://github.com/neovim/neovim/blob/master/src/nvim/buffer.c#L129',
+                expected_target = {
+                    repo = 'neovim',
+                    branch = 'master',
+                    file = 'src/nvim/buffer.c',
+                    line = 129,
+                },
+            },
+            {
+                url = 'https://github.com/charmbracelet/bubbletea/blob/main/tea.go#L45',
+                expected_target = {
+                    repo = 'bubbletea',
+                    branch = 'main',
+                    file = 'tea.go',
+                    line = 45,
+                },
+            },
+        },
+    }
+    it('Extracts path elements', function()
+        for _, case in ipairs(example_links) do
+            local results = explode_link(case.url)
+            assert.same(case.expected_target, results)
+        end
     end)
 end)
