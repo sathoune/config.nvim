b81fdd4987b476bf84f86ccd51636b52d31eb7f5
sathoune <michal5bargiel@gmail.com>
2025-07-28 21:36:44 +0200

    Test with a custom keymap store.

--- lua/custom/instrumentation/read_mappings_spec.lua
+++ lua/custom/instrumentation/read_mappings_spec.lua
@@ -1,4 +1,4 @@
-local function list_keymaps()
+local function list_keymaps(function_that_fetches_keymaps)
     local modes = {
         'n',
         'v',
@@ -10,16 +10,36 @@ local function list_keymaps()
         'R', -- replace
     }
     local result_table = {}
+
     for _, mode in ipairs(modes) do
-        local mode_keymaps = vim.api.nvim_get_keymap(mode)
-        result_table[mode] = mode_keymaps
+        result_table[mode] = function_that_fetches_keymaps(mode)
     end
+
     return result_table
 end
 
+local function my_keymap_getter_factory(keymap)
+    local function keymap_getter(mode)
+        return keymap[mode]
+    end
+    return keymap_getter
+end
+
 describe('List the keymaps', function()
-    it('Gathers them all for me', function()
-        local keymaps = list_keymaps()
+    it('Gathers keymaps with nvim api', function()
+        local nvim_function_for_keymaps = vim.api.nvim_get_keymap
+        local keymaps = list_keymaps(nvim_function_for_keymaps)
         print(vim.inspect(keymaps))
+    end)
+
+    it('Gathers keymaps with my own function', function()
+        local custom_keymap = {
+            n = { { lhs = 'd', rhs = 'abc' } },
+            i = { { lhs = 'e', rhs = 'cde' } },
+        }
+        local keymap_getter = my_keymap_getter_factory(custom_keymap)
+        local found_keymaps = list_keymaps(keymap_getter)
+
+        assert.same(custom_keymap, found_keymaps)
     end)
 end)
